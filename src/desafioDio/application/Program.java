c423eca3b87fe310832d9f54a39af05c12ec1651
jonh-dev <jonh.dev.br@gmail.com>
2022-07-23 01:18:47 -0300

    Create entities and application

---
+++ src/desafioDio/application/Program.java
@@ -0,0 +1,32 @@
+package desafioDio.application;
+
+import desafioDio.entities.Curso;
+import desafioDio.entities.Mentoria;
+
+import java.time.LocalDate;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição do curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Js");
+        curso2.setDescricao("Descrição do curso Js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição mentoria de Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+    }
+}
